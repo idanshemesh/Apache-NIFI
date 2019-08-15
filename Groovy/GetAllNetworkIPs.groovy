a36fb52cef24c83952d87ca8bb6fcb2d305976a7
Idan Shemesh <idan.accounts@gmail.com>
2019-08-15 15:18:23 +0300

    Create GetAllNetworkIPs.groovy

---
+++ Groovy/GetAllNetworkIPs.groovy
@@ -0,0 +1,36 @@
+//////////////////////////////////////////////////
+// This script loop on all network cards and    //
+// put them in folwfile attribute:              //
+// host.ip.N     - the ip                       //
+// host.ip.count - Numer of IPs                 //
+//                                              //
+// Git: https://github.com/idanshemesh          //
+//                                              //
+//////////////////////////////////////////////////
+
+import java.io.*
+import java.net.*
+import java.util.*
+
+def flowFile = session.get()
+if(!flowFile) return
+
+def text = ''
+def counter = 0
+//Get all my network cards IPs
+def interfaces = NetworkInterface.getNetworkInterfaces()
+while (interfaces.hasMoreElements()) {
+    def addresses = interfaces.nextElement().getInetAddresses()
+    while (addresses.hasMoreElements()) {
+        def ip = addresses.nextElement().getHostAddress()
+        if (ip != '127.0.0.1') {
+         flowFile = session.putAttribute(flowFile, 'host.ip.' + counter, ip)
+         counter++
+        }
+    }
+}
+
+
+flowFile = session.putAttribute(flowFile, 'host.ip.count', counter.toString())
+
+session.transfer(flowFile, REL_SUCCESS)
